3748b67246e656330a916a1cdf16b4a53506cb69
dhryn <102226490+dhryn@users.noreply.github.com>
2023-11-26 11:43:25 +0700

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -56,7 +56,7 @@ const Main = () => {
   };
 
   const fetchData = () => {
-    fetch('http://localhost:3000/api/gamesData.json')
+    fetch('https://my-json-server.typicode.com/dhryn/ta-ppb/games')
       .then((res) => res.json())
       .then((data) => {
         setGames(data);
